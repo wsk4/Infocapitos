1428a0f7bc042b280721fdd0ef56db8603151672
wsk4 <maa.gonzalezva@duocuc.cl>
2025-11-20 22:35:38 -0300

    kk

--- app/src/main/java/com/example/infocapitos/navigation/BottomNavNoticia.kt
+++ app/src/main/java/com/example/infocapitos/navigation/BottomNavNoticia.kt
@@ -6,15 +6,18 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.*
 import androidx.compose.ui.graphics.vector.ImageVector
 
 sealed class BottomNavNoticia(val route: String, val label: String, val icon: ImageVector) {
+
     object Home : BottomNavNoticia(Routes.HOME, "Home", Icons.Default.Home)
     object Add : BottomNavNoticia(Routes.ADD, "Agregar", Icons.Default.Add)
+    object Picture : BottomNavNoticia(Routes.PICTURE,"Foto", Icons.Default.Face)
     object Profile : BottomNavNoticia(Routes.PROFILE, "Perfil", Icons.Default.Person)
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun BottomBar(navController: NavHostController, noticias: List<BottomNavNoticia>) {
     val navBackStackEntry by navController.currentBackStackEntryAsState()
@@ -23,8 +26,8 @@ fun BottomBar(navController: NavHostController, noticias: List<BottomNavNoticia>
     NavigationBar {
         noticias.forEach { noticia ->
             NavigationBarItem(
-                icon = { androidx.compose.material3.Icon(noticia.icon, contentDescription = noticia.label) },
-                label = { androidx.compose.material3.Text(noticia.label) },
+                icon = { Icon(noticia.icon, contentDescription = noticia.label) },
+                label = { Text(noticia.label) },
                 selected = currentRoute == noticia.route,
                 onClick = {
                     if (currentRoute != noticia.route) {
